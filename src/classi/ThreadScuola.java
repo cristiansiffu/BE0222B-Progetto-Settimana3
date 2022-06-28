842429cd44a3a847016b4dd019b66fb0c0876fe2
cristiansiffu <csiffu96@gmail.com>
2022-06-28 18:22:44 +0200

    first commit

---
+++ src/classi/ThreadScuola.java
@@ -0,0 +1,30 @@
+package classi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import main.Main;
+
+public class ThreadScuola implements Runnable {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ThreadScuola.class);
+	private Scuola scuola;
+
+	public ThreadScuola(Scuola scuola) {
+		this.scuola = scuola;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < scuola.getStudenti().size(); i++) {
+			logger.info(scuola.getStudente(i).toString());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
